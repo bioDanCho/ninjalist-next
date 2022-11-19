b5bdd18b98f37c94eedab85dddd4351423f9a44c
bioDanCho <daniel.cho@thebioagency.com>
2022-11-19 01:26:17 -0500

    lesson 11

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../../styles/Ninjas.module.css';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -17,11 +18,13 @@ const Ninjas = ({ ninjas }) => {
         <div>
             <h1>All Ninjas</h1>
             {ninjas.map((ninja) => (
-                <div key={ninja.id}>
-                    <a className={styles.single} href=''>
-                        <h3>{ninja.name}</h3>
-                    </a>
-                </div>
+                <Link
+                    className={styles.single}
+                    href={`/ninjas/${ninja.id}`}
+                    key={ninja.id}
+                >
+                    <h3>{ninja.name}</h3>
+                </Link>
             ))}
         </div>
     );
