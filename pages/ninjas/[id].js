aad96d7afb834fdfbaf36b7b5a7926f520790df4
bioDanCho <daniel.cho@thebioagency.com>
2022-11-19 01:33:18 -0500

    lesson 12

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map((user) => {
+        return {
+            params: {
+                id: user.id.toString(),
+            },
+        };
+    });
+
+    return {
+        paths: paths,
+        fallback: false,
+    };
+};
+
 const Details = () => {
     return (
         <div>
