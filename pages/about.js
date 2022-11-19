d94f462185db96594e087b9174c84382b4d404bb
bioDanCho <daniel.cho@thebioagency.com>
2022-11-19 01:04:08 -0500

    lesson 9

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,29 @@
+import Head from 'next/head';
 import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                totam est, mollitia perferendis, facilis molestias suscipit esse
-                blanditiis praesentium fugiat quidem possimus odio nemo! Facilis
-                perspiciatis pariatur nesciunt qui sint?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                totam est, mollitia perferendis, facilis molestias suscipit esse
-                blanditiis praesentium fugiat quidem possimus odio nemo! Facilis
-                perspiciatis pariatur nesciunt qui sint?
-            </p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus totam est, mollitia perferendis, facilis molestias
+                    suscipit esse blanditiis praesentium fugiat quidem possimus
+                    odio nemo! Facilis perspiciatis pariatur nesciunt qui sint?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus totam est, mollitia perferendis, facilis molestias
+                    suscipit esse blanditiis praesentium fugiat quidem possimus
+                    odio nemo! Facilis perspiciatis pariatur nesciunt qui sint?
+                </p>
+            </div>
+        </>
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,34 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
     return (
-        <div>
-            <h1 className={styles.title}>Homepage</h1>
-            <p className={styles.text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
-                vel. Obcaecati consequatur distinctio, dicta nam eius,
-                dignissimos cupiditate alias recusandae cumque dolorem
-                consectetur blanditiis. Possimus saepe veniam qui libero eius.
-            </p>
-            <p className={styles.text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
-                vel. Obcaecati consequatur distinctio, dicta nam eius,
-                dignissimos cupiditate alias recusandae cumque dolorem
-                consectetur blanditiis. Possimus saepe veniam qui libero eius.
-            </p>
-            <Link className={styles.btn} href='/ninjas'>
-                See Ninja Listing
-            </Link>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | Home</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1 className={styles.title}>Homepage</h1>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum, vel. Obcaecati consequatur distinctio, dicta nam eius,
+                    dignissimos cupiditate alias recusandae cumque dolorem
+                    consectetur blanditiis. Possimus saepe veniam qui libero
+                    eius.
+                </p>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum, vel. Obcaecati consequatur distinctio, dicta nam eius,
+                    dignissimos cupiditate alias recusandae cumque dolorem
+                    consectetur blanditiis. Possimus saepe veniam qui libero
+                    eius.
+                </p>
+                <Link className={styles.btn} href='/ninjas'>
+                    See Ninja Listing
+                </Link>
+            </div>
+        </>
     );
 }
