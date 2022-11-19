c776f8e3efc194f35aca31ba191d60685692fe3f
bioDanCho <daniel.cho@thebioagency.com>
2022-11-19 00:35:51 -0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                totam est, mollitia perferendis, facilis molestias suscipit esse
+                blanditiis praesentium fugiat quidem possimus odio nemo! Facilis
+                perspiciatis pariatur nesciunt qui sint?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                totam est, mollitia perferendis, facilis molestias suscipit esse
+                blanditiis praesentium fugiat quidem possimus odio nemo! Facilis
+                perspiciatis pariatur nesciunt qui sint?
+            </p>
+        </div>
+    );
+};
+
+export default About;
